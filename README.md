ee964983ccfa1d33e2eb59c4e1722cded5dd6ccb
Jaser Akuly <jaser@therrd.com>
2017-10-29 12:25:43 -0500

    added crypto-js module, readme, and block class

---
+++ README.md
@@ -0,0 +1,3 @@
+Blockchain with JavaScript. 
+
+It's not a complete implementation, but enough to understand how blockchains work and how they guarantee that blocks can never be changed. After creating our blockchain I tried and tamper with it. This will demonstrate a blockchains ability to detect fraud.
--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+
+const SHA256 = require('crypto-js/sha256');
+
+
+// Key Terms Defined:
+//Index = optional, where the block sits on the chain
+//Timestamp = when was the block was created 
+//Data = any type of data to be assosiated, IE. If currency, the transaction information
+//PreviousHash = string that contains the hash of the block before this one, **Very important as it keeps itengrity in line. 
+
 class Block {
     constructor(index, timestamp, data, previousHash = "") {
-        
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.has = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
